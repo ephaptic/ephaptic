166e25ce68333e2b161bbe62c5c292b5f822e8bf
uukelele-scratch <robustrobot11@gmail.com>
2025-12-14 15:57:08 +0000

    fix(python/cli): clarify what is happening and add more user messages

--- packages/python/ephaptic/cli/__main__.py
+++ packages/python/ephaptic/cli/__main__.py
@@ -11,11 +11,14 @@ app = typer.Typer(help="Ephaptic CLI tool.")
 def load_ephaptic(import_name: str) -> Ephaptic:
     sys.path.insert(0, os.getcwd())
 
-    if ":" not in import_name: import_name += ":app" # default: expect app to be named `app` inside the file
+    if ":" not in import_name:
+        typer.secho(f"Warning: Import name did not specify app name. Defaulting to `app`.", fg=typer.colors.YELLOW)
+        import_name += ":app" # default: expect app to be named `app` inside the file
 
     module_name, var_name = import_name.split(":", 1)
 
     try:
+        typer.secho(f"Attempting to import `{var_name}` from `{module_name}`...")
         module = importlib.import_module(module_name)
     except ImportError as e:
         typer.secho(f"Error: Can't import '{module_name}'.\n{e}", fg=typer.colors.RED)
@@ -35,7 +38,7 @@ def load_ephaptic(import_name: str) -> Ephaptic:
 
 @app.command()
 def generate(
-    app: str = typer.Argument('app:app', help="The import string (e.g. `app:app`)"),
+    app: str = typer.Argument('app:app', help="The import string. (Default: `app:app`)"),
     output: Path = typer.Option('schema.json', '--output', '-o', help="Output path for the JSON schema.")
 ):
     ephaptic = load_ephaptic(app)
